2a04e442067979848430c0c7706e6b9a4f24b84c
solo-mizan <mizanbd.eee@gmail.com>
2022-04-07 00:54:56 +0600

    google pop up sign up added

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrJuGz_J2W7WOaLSgf135J2D-ZOtj3BkQ",
+  authDomain: "simple-firebase-authenti-3750a.firebaseapp.com",
+  projectId: "simple-firebase-authenti-3750a",
+  storageBucket: "simple-firebase-authenti-3750a.appspot.com",
+  messagingSenderId: "693564592224",
+  appId: "1:693564592224:web:c2c388ad2c96effcb0f305"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
